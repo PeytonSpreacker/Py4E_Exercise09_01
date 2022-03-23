e4e8bfb7d0d8fb43cd45b3d74979a4db729b21fe
Your Name <peyton@spreacker.com>
2022-03-22 20:39:35 -0500

    final commit

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,13 @@
 #open file for use in this code using dialog
 fhand = open(input('Enter file name here: '))
-#prepare the file lines and create the word list
-for line in fhand:
-    strip_line = line.rstrip('\n')
-    print(strip_line)
-    if len(strip_line) < 1:
-        continue
-    else:
-        words = strip_line.split()
-#count the words in the file by iteration
+#always make the dictionary in front of the working block of code
 counts = dict()
-for word in words:
-    counts[word] = counts.get(word, 0) + 1
+for line in fhand:
+    line = line.rstrip()
+    words = line.split()
+    #the get() function searches the existing dictionary for the word. If the word is present, then one iteration is added to the existing value.
+    #if the word is not present, get() makes the default value 0 and adds one
+    for w in words:
+        counts[w] = counts.get(w,0) + 1
+        #make sure you use [] when working with dictionaries!!
 print(counts)
